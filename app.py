310bcef0a1567e15c4c10f3c3b372abba7ab8a32
Shubrah Gupta <50666757+shubrahgupta@users.noreply.github.com>
2023-03-16 02:22:07 +0530

    Corrected the error caused by textarea
    Only thing left is to relay the output on the same html page

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import numpy as np
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request, Depends
 import pandas as pd
 from starlette.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import model
 import re
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import ORJSONResponse, UJSONResponse
 
 def parseInt(text):
     arr = [int(s) for s in re.findall(r'\d+', text)]
@@ -15,8 +20,15 @@ def parseInt(text):
     except IndexError:
         return 0
 
+class Query(BaseModel):
+    log:str
+    response:str
+    tag:str
+    amount:int
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='templates')
 
 
 @app.get('/')  # basic get view
@@ -24,14 +36,16 @@ def basic_view():
     return {"WELCOME": "GO TO /docs route, or /post or send POST request to /entry "}
 
 @app.get('/entry', response_class=HTMLResponse)
-def take_input():
-    return '''
-        <form method="post">
-        <input maxlength="28" name="text" type="text" />
-        <input type="submit" />'''
+def take_input(request: Request):
+    return templates.TemplateResponse('index.html',{'request': request})
+    
+    # return '''
+    #     <form method="post">
+    #     <input maxlength="28" name="text" type="text" />
+    #     <input type="submit" />'''
 
 @app.post('/entry')
-def input_analysis(text: str = Form(...)):
+async def input_analysis(text: str = Form(...)):
     loaded_model = tf.keras.models.load_model('budgetMaster_chatbot.h5')
 
     intents = model.Pclass(text, model.newWords, model.ourClasses, loaded_model)
@@ -39,9 +53,8 @@ def input_analysis(text: str = Form(...)):
     # ourResult.append(text)
     ourResult["log"] = text
     ourResult["amount"] = parseInt(text)
-
-
     return ourResult
+
 
 
 
